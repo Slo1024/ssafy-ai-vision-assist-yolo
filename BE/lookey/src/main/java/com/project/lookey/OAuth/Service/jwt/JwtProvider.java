e1a1d77d9a6437eaa163c2ac0087d8f2ddf3f278
ttangju02 <ttangju02@gmail.com>
2025-09-13 21:24:13 +0900

    [BE] fix: 구글 소셜 로그인 jwt 토근 길ì
    - HS512용 키 생성 방법 수정

--- BE/lookey/src/main/java/com/project/lookey/OAuth/Service/jwt/JwtProvider.java
+++ BE/lookey/src/main/java/com/project/lookey/OAuth/Service/jwt/JwtProvider.java
@@ -3,12 +3,14 @@ package com.project.lookey.OAuth.Service.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -16,6 +18,11 @@ public class JwtProvider {
 
     @Value("${jwt.secret}")
     private String secretKey;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey); // Base64 디코딩
+        return Keys.hmacShaKeyFor(keyBytes); // HS512용 Key 생성
+    }
 
     // Access Token: 30분
     private final long ACCESS_TOKEN_EXPIRATION_MS = 1000L * 60 * 30;
